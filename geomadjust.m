8a348c27132f3b3b995e04384acf2a71a0558d8c
_chris <ccampb19@nd.edu>
2019-02-27 10:22:55 -0500

    rotating fiber ferrule a bit

--- geomadjust.m
+++ geomadjust.m
@@ -9,15 +9,15 @@ for ii = 1:6
         switch ii
 %             case {1}
                 %B/W ;marking tape, center fiber: do nothing
-            case {1,2}
-                %Blue tape, fiber at 8 (or 10) o'clock
-                NEWRHOS(ii,:) = sqrt((RHOS+0.45).^2+0.25.^2);
-            case {3,6}
-                %Green or red tape, fiber at 6 or 12 o'clock
-                NEWRHOS(ii,:) = sqrt(RHOS.^2+.45^2);
-            case {4,5}
-                %Yellow or orange, fiber at 2 or 4 o'clock
-                NEWRHOS(ii,:) = sqrt((RHOS-0.45).^2+0.25^2);
+            case {2,3}
+                %Green or blue tape, fiber at 8 or 10 o'clock
+                NEWRHOS(ii,:) = sqrt((RHOS+0.35).^2+0.25.^2);
+            case {1,4}
+                %Brown or yellow tape, fiber at 6 or 12 o'clock
+                NEWRHOS(ii,:) = sqrt(RHOS.^2+.46^2);
+            case {5,6}
+                %Orange or red tape, fiber at 2 or 4 o'clock
+                NEWRHOS(ii,:) = sqrt((RHOS-0.35).^2+0.25^2);
             otherwise
                 error('Something Wrong...')
         end
