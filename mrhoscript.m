e90aee9a53178bfe21ad2543b1b13e568ff75a01
_chris <ccampb19@nd.edu>
2019-07-10 16:17:29 -0400

    ''

--- mrhoscript.m
+++ mrhoscript.m
@@ -2,7 +2,7 @@
 clear
 % close all
 
-opt.basedir = 'D:\fdpm.data\il_mdseries\190528\';
+opt.basedir = 'D:\fdpm.data\bovine_deox\190709\';
 opt.nind = 1.33; %1.33 for water, 1.36 for 20% intralipid, 1.4 for PDMS  
 opt.laser_names=[630,660,688,781,828,848];  %only for plotting names
 opt.usediodes = 1:6;
@@ -17,14 +17,14 @@ opt.bbdark = 1;
 opt.sphname = 'sphere';
 opt.sphreps = -1;
 opt.smooth = 1;
-opt.threshold = 4000; % Counts where uncalibrated spectrometer loses linearity
+opt.threshold = 6000; % Counts where uncalibrated spectrometer loses linearity
 
 % Load all possible rhos, to be adjusted in endmat (use long rhos if
 % possible)
 startrho = 12;
 % With more dynamic range, broadband needs lower rhos for adequate counts
 bbstartrho = 11;
-endrho = 25;
+endrho = 28;
 opt.rhosteps = 1;
 opt.rhorange = startrho:opt.rhosteps:endrho;
 opt.bbrhorange = bbstartrho:opt.rhosteps:endrho;
@@ -34,7 +34,7 @@ endfreq = 560;
 % Code will replace the rho in the prototype with opt.rhorange.
 % Ex: 'ex-12-1-baseline' will load 'ex-12-1-baseline-dcswitch.asc' thru 
 %     'ex-30-1-baseline-dcswitch.asc'
-base = '0mg_nigros';
+base = 'blood_hio2_good';
 opt.filenameprototype = [num2str(startrho) '-1-' base ];
 
 % fdpm dark msmts
