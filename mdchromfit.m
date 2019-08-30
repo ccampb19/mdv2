dd2dd75ce2933b122401e2e0a6c9d6be786a0d50
_chris <ccampb19@nd.edu>
2019-08-30 13:18:34 -0400

    add chromophore fit

--- mdchromfit.m
+++ mdchromfit.m
@@ -1,26 +1,36 @@
-function [fit,concs,wvrange] = mdchromfit(phantom,chromfile)
+function [chromnames,fit,concs,wvrange] = mdchromfit(phantom,chromfile,uchrom)
 
 temp = importdata(chromfile);
 chromwv = temp.data(:,1);
-chromdata = temp.data(:,2:end);
-chromnames=temp.colheaders(2:end)';
+uchrom = logical([0,uchrom]);
+assert(length(uchrom) == size(temp.data,2),...
+    'Error: usechroms has wrong length');
+    
+chromdata = temp.data(:,uchrom);
+chromnames=temp.colheaders(uchrom)';
 
 wvrange = phantom(:,1);
 A = interp1(chromwv,chromdata,wvrange);
 pidxs = ~isnan(phantom(:,2)) & ~isnan(A(:,1));
 muawv = phantom(pidxs,1);
 %Don't fit past 910nm
-testwv = find(muawv>910,1,'first');
+testwv = find(muawv>925,1,'first');
 if isempty(testwv)
     testwv = length(mua);
 end
 pidxs(testwv:end) = 0;
+testwv = find(muawv<620,1,'last');
+if isempty(testwv)
+    testwv = 0;
+end
+pidxs(1:testwv) = 0;
 
 mua = phantom(pidxs,2);
 muawv = phantom(pidxs,1);
 
 concs = lsqnonneg(A(pidxs,:),mua);
 fit = chromdata*concs;
+
 
 figure
 plot(muawv,mua,chromwv,fit)
