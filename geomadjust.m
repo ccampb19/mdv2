d1dab00760c4eef70e5df3ad90e734ebf860f535
_chris <ccampb19@nd.edu>
2022-02-10 14:21:01 -0500

    A couple quick changes 	- Add license 	- Reverse ferrule orientation 	- Remove unused model

--- geomadjust.m
+++ geomadjust.m
@@ -4,20 +4,21 @@ function NEWRHOS = geomadjust(USEDIODES,RHOS)
 %   NEWRHOS are adjusted for ferrule geometry.
 %   At this point, geometry is hard-coded in this function.
 
+%   Current configuration:  _Dets_   <-Sources->
+%   Broadband is center fiber (B/W tape)
+
 NEWRHOS = zeros(length(USEDIODES),length(RHOS));
 for ii = 1:6
         switch ii
-%             case {1}
-                %B/W ;marking tape, center fiber: do nothing
             case {2,3}
                 %Green or blue tape, fiber at 8 or 10 o'clock
-                NEWRHOS(ii,:) = sqrt((RHOS+0.433).^2+0.25.^2);
+                NEWRHOS(ii,:) = sqrt((RHOS-0.433).^2+0.25.^2);
             case {1,4}
                 %Brown or yellow tape, fiber at 6 or 12 o'clock
                 NEWRHOS(ii,:) = sqrt(RHOS.^2+.5^2);
             case {5,6}
                 %Orange or red tape, fiber at 2 or 4 o'clock
-                NEWRHOS(ii,:) = sqrt((RHOS-0.433).^2+0.25^2);
+                NEWRHOS(ii,:) = sqrt((RHOS+0.433).^2+0.25^2);
             otherwise
                 error('Something Wrong...')
         end
